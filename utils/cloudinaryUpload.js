3c7badd19e024ef4a24fb602a89993bc2fcee810
huzaifashahid742 <huzaifashahid742@gmail.com>
2026-08-18 18:33:02 +0500

    update

--- utils/cloudinaryUpload.js
+++ utils/cloudinaryUpload.js
@@ -1,4 +1,4 @@
-import cloudinary from './cloudinary.js'; // Import your configured instance (adjust path if needed)
+import cloudinary from '../Config/cloudinary.js'; // Import your configured instance (adjust path if needed)
 
 export const uploadToCloudinary = (fileBuffer, folder = "university-logos") => {
     return new Promise((resolve, reject) => {
