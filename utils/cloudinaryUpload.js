08b2975a60779286f1c8259e24170ae8631e88dd
huzaifashahid742 <huzaifashahid742@gmail.com>
2026-08-18 16:23:32 +0500

    update

--- utils/cloudinaryUpload.js
+++ utils/cloudinaryUpload.js
@@ -4,7 +4,10 @@ import streamifier from 'streamifier';
 export const uploadToCloudinary = (fileBuffer, folderName = "airease-uploads") => {
   return new Promise((resolve, reject) => {
     const uploadStream = cloudinary.uploader.upload_stream(
-      { folder: folderName },
+      { 
+        folder: folderName, 
+        resource_type: "auto" // <-- Add this line to correctly process PDFs, images, etc.
+      },
       (error, result) => {
         if (result) {
           resolve(result);
