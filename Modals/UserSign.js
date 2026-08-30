8d96ec288d92125e1e4169ce20bbff02762bde91
huzaifashahid742 <huzaifashahid742@gmail.com>
2026-08-30 19:47:18 +0500

    update

--- Modals/UserSign.js
+++ Modals/UserSign.js
@@ -42,7 +42,7 @@ const userSchema = new mongoose.Schema(
         },
         password: {
             type: String,
-            required: [true, "Password is required"],
+            // required: [true, "Password is required"],
             minlength: [6, "Password must be at least 6 characters long"],
         },
         role: {
