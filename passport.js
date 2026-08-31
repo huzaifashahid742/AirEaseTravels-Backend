824bb1569e5b83cc8b079f4e3fcb5414de0d7663
huzaifashahid742 <huzaifashahid742@gmail.com>
2026-08-31 12:59:01 +0500

    update

--- passport.js
+++ passport.js
@@ -15,7 +15,8 @@ const generateJWT = (user) => {
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: process.env.GOOGLE_CALLBACK_URL
+    callbackURL: process.env.GOOGLE_CALLBACK_URL;
+    prompt: 'select_account'
   },
   async (accessToken, refreshToken, profile, done) => {
     try {
